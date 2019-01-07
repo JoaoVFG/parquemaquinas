92fb508b586af66a904b1ca47fd66db357473d31
JoaoVFG <joao.vfg@hotmail.com.br>
2019-01-07 09:54:01 -0200

    Realizada a configuração das dependencias  e conexão com o Banco de Dados

---
+++ src/main/java/joaovfg/com/github/ParqueMaquinasApplication.java
@@ -0,0 +1,14 @@
+package joaovfg.com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueMaquinasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueMaquinasApplication.class, args);
+	}
+
+}
+

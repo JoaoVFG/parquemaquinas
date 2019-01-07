0daf378a2c7f9b5afb0733e8d6157414a858079b
JoaoVFG <joao.vfg@hotmail.com.br>
2019-01-07 10:21:50 -0200

    Desenvolvido repositorio para computador

---
+++ src/main/java/joaovfg/com/github/repositories/ComputadorRepository.java
@@ -0,0 +1,18 @@
+package joaovfg.com.github.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import joaovfg.com.github.entity.Computador;
+
+public interface ComputadorRepository extends JpaRepository<Computador, Integer>{
+	
+	@Transactional(readOnly = true)
+	@Query("SELECT computador FROM Computador computador WHERE computador.id = :id")
+	public Computador buscaPorId(@Param("id") Integer id);
+	
+	
+
+}
